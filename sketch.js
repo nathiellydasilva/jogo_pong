0e5dbaf854cc419a5e51e40e779a3dbadce2241c
nathielly da silva <106687664+nathiellydasilva@users.noreply.github.com>
2022-06-29 10:52:56 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+// Draw a circle at location (30, 30) with a diameter of 20.
+circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+}  
